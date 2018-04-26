d46c59f8ed36c9d32549904eac702fdec1b8268e
AROMAN3D <38281246+AROMAN3D@users.noreply.github.com>
2018-04-26 12:17:50 +0200

    Files from gitlab
    Files from the commit on gitlab

---
+++ CVReadout.java
@@ -0,0 +1,70 @@
+/*
+ * CVReadout.java
+ * 
+ * Copyright 2018 arnold.slotboom <arnold.slotboom@>
+ * 
+ * 
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+ 
+public class CVReadout
+{
+ 
+    private static Socket socket;
+ 
+    public static void main(String args[])
+    {
+        try
+        {
+            String host = "localhost";
+            int port = 25000; //Arnold: port given by jar start
+            InetAddress address = InetAddress.getByName(host);
+            socket = new Socket(address, port);
+ 
+            //Send the message to the server
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+ 
+            String number = "2"; //Arnold: startup send to cv.
+ 
+            String sendMessage = number + "\n";
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message sent to the server : "+sendMessage);
+ 
+            //Get the return message from the server
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine(); // Arnold output of cv jar.
+            System.out.println("Message received from the server : " +message);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            //Closing the socket
+            try
+            {
+                socket.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+ 
+ 
+ 
